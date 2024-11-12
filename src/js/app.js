aef3b05cf58ed11ec8ebd0f140c4e498cbe3e2cc
Antonio Gomez Gallardo <antoniogomezgallardo@gmail.com>
2024-11-12 18:25:01 +0100

    Added dark mode

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
     eventListener();
+
+    toggleMode();
 });
 
 function eventListener() {
     const mobileMenu = document.querySelector('.mobile-menu');
-
     mobileMenu.addEventListener('click', toggleMenu);
 }
 
@@ -16,4 +17,43 @@ function toggleMenu() {
     else
         navegacion.classList.add('mostrar')
 
+}
+
+function toggleMode() {
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme-dark)');
+    if (prefiereDarkMode.matches) {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+        
+    prefiereDarkMode.addEventListener('change', function() {
+        if (prefiereDarkMode.matches) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    });
+ 
+    //console.log(prefiereDarkMode.matches);
+        //Boton DarkMode
+    const botonDarkMode = document.querySelector('.dark-mode-button');
+    botonDarkMode.addEventListener('click', function(){
+        document.body.classList.toggle('dark-mode'); 
+ 
+        //Para que el modo elegido se quede guardado en local-storage
+        if (document.body.classList.contains('dark-mode')) {
+            localStorage.setItem('modo-oscuro','true');
+        } else {
+            localStorage.setItem('modo-oscuro','false');
+        }
+    });
+ 
+    //Obtenemos el modo del color actual
+    if (localStorage.getItem('modo-oscuro') === 'true') {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+    
 }
